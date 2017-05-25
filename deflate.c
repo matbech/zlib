0839525eb787018aefe68d685bd59bda8c6085d5
Mat Berchtold <mberchtold@gmail.com>
2017-05-25 15:52:14 +0200

    added more notes for TZCNT. use _BitScanForward for ARM

--- deflate.c
+++ deflate.c
@@ -1175,14 +1175,29 @@ local void lm_init (s)
 #if defined(_MSC_VER) && !defined(__clang__)
 #include <intrin.h>
 /* __builtin_ctzl
- *  For 0, the result is undefined
- *  the equivalent intrinsic on MSC is _BitScanForward
+ *  - For 0, the result is undefined
+ *  - On the x86 architecture, it is typically implemented using BSF
+ *  - the equivalent intrinsic on MSC is _BitScanForward
+ *
  * _tzcnt_u32
- *  For 0, the result is the size of the operand 
- *  On processors that do not support TZCNT, the instruction byte encoding is executed as BSF. In this case the result for 0
- *  is undefined.
+ *  - For 0, the result is the size of the operand 
+ *  - On processors that do not support TZCNT, the instruction byte encoding is executed as BSF. In this case the result for 0
+ *    is undefined.
+ *  - Performance:
+ *    + AMD: The reciprocal throughput for TZCNT is 2 vs 3 for BSF
+ *    + Intel: On modern Intel CPUs (Haswell), the performance of TZCNT is equivalent to BSF
+ *    Reference: http://www.agner.org/optimize/instruction_tables.pdf
 */
+#if defined(_M_IX86) || defined(_M_AMD64)
 #define __builtin_ctzl _tzcnt_u32
+#else
+static __forceinline unsigned long __builtin_ctzl(unsigned long value)
+{
+    unsigned long trailing_zero;
+    _BitScanForward(&trailing_zero, value);
+    return trailing_zero;
+}
+#endif
 #endif
 
 /* ===========================================================================
